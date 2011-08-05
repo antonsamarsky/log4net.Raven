728d2a39dca9614240b947f6869136301f7ad8e8
asa <asa.idev@gmail.com>
2011-08-05 12:37:59 +0300

    Fulfill appender with functionality. Initial tests.

--- log4net.Raven/log4net.Raven.Tests/LoggerTests.cs
+++ log4net.Raven/log4net.Raven.Tests/LoggerTests.cs
@@ -1,10 +1,49 @@
 ﻿using NUnit.Framework;
+using Raven.Client;
 
 namespace log4net.Raven.Tests
 {
 	[TestFixture]
 	public class LoggerTests
 	{
+		private ILog log;
 
+		private RavenAppender appender;
+
+		private IDocumentSession documentSession;
+
+		[TestFixtureSetUp]
+		public void TestFixtureSetUp()
+		{
+			this.log = LogManager.GetLogger(this.GetType());
+
+			Config.XmlConfigurator.Configure();
+
+			var appenders = log.Logger.Repository.GetAppenders();
+			Assert.IsTrue(appenders.Length > 0, "Seems that Raven Appender is not configured");
+
+			appender = appenders[0] as RavenAppender;
+			Assert.IsNotNull(appender, "Raven Appender is expected to be the only one configured for tests");
+
+			documentSession = appender.DocumentSession;
+		}
+
+		[TestFixtureTearDown]
+		public void TestFixtureTearDown()
+		{
+			// ClearCollection();
+			LogManager.Shutdown();
+		}
+
+		[Test]
+		public void SmokeLogTest()
+		{
+			log.Info("Log Info");
+		}
+
+		protected void ClearCollection()
+		{
+			//documentSession.
+		}
 	}
 }
--- log4net.Raven/log4net.Raven/RavenAppender.cs
+++ log4net.Raven/log4net.Raven/RavenAppender.cs
@@ -1,4 +1,5 @@
-﻿using Raven.Client;
+﻿using System;
+using Raven.Client;
 using Raven.Client.Document;
 using log4net.Appender;
 using log4net.Core;
@@ -10,37 +11,133 @@ namespace log4net.Raven
 	/// </summary>
 	public class RavenAppender : AppenderSkeleton
 	{
-		public RavenAppender()
+		private readonly object lockObject= new object();
+
+		private string databaseName = "Logs"; // Default Database Name
+
+		//private int messageCounter = 1; // Default number of log entries to be commited.
+
+		#region Appender configuration properties
+
+		public string ConnectionString { get; set; }
+
+		public string DatabaseName
 		{
-			this.Url = @"http://localhost";
+			get { return this.databaseName; }
+			set { this.databaseName = value; }
 		}
 
-		public RavenAppender(string connectionStringName)
-		{
-			this.ConnectionStringName = connectionStringName;
-		}
+		// public string CollectionName { get; set; }
 
-		public RavenAppender(string connectionStringName, string databaseName)
-		{
-			this.ConnectionStringName = connectionStringName;
-			this.DatabaseName = databaseName;
-		}
+		#endregion
 
-		public string Url { get; set; }
+		public IDocumentSession DocumentSession { get; protected set; }
 
-		public string ConnectionStringName { get; set; }
-
-		public string DatabaseName { get; set; }
-
-		public string CollectionName { get; set; }
-
-		public IDocumentSession DocumentSession { get; private set; }
-
-		public DocumentStore DocumentStore { get; private set; }
+		protected DocumentStore DocumentStore { get; set; }
 
 		protected override void Append(LoggingEvent loggingEvent)
 		{
+			this.CheckSession();
+
 			var entry = new LogEntry(loggingEvent);
+			this.DocumentSession.Store(entry);
+
+			this.Commit();
+		}
+
+		public override void ActivateOptions()
+		{
+			try
+			{
+				this.InitServer();
+			}
+			catch (Exception e)
+			{
+				ErrorHandler.Error("Exception while initializing Raven Appender", e, ErrorCode.GenericFailure);
+			}
+		}
+
+		protected override void OnClose()
+		{
+			this.Commit();
+
+			if (this.DocumentSession != null)
+			{
+				this.DocumentSession.Dispose();
+			}
+
+			if (this.DocumentStore != null)
+			{
+				this.DocumentStore.Dispose();
+			}
+
+			base.OnClose();
+		}
+
+		protected virtual void Commit()
+		{
+			if (this.DocumentSession == null)
+			{
+				return;
+			}
+
+			this.DocumentSession.SaveChanges();
+		}
+
+		/// <summary>
+		/// IDocumentSession - Instances of this interface are created by the DocumentStore, 
+		/// they are cheap to create and not thread safe. 
+		/// If an exception is thrown by an IDocumentSession method, 
+		/// the behavior of all of the methods (except Dispose) is undefined.
+		/// The document session is used to interact with the Raven database, 
+		/// load data from the database, query the database, save and delete. 
+		/// Instances of this interface implement the Unit of Work pattern and change tracking.
+		/// </summary>
+		private void CheckSession()
+		{
+			if (this.DocumentSession != null)
+			{
+				return;
+			}
+
+			lock (this.lockObject)
+			{
+				if (this.DocumentSession != null)
+				{
+					return;
+				}
+
+				this.DocumentSession = this.DocumentStore.OpenSession();
+				this.DocumentSession.Advanced.UseOptimisticConcurrency = true;
+			}
+		}
+
+		/// <summary>
+		/// IDocumentStore - This is expensive to create, 
+		/// thread safe and should only be created once per application. 
+		/// The Document Store is used to create DocumentSessions, 
+		/// to hold the conventions related to saving/loading data and 
+		/// any other global configuration. 
+		/// </summary>
+		private void InitServer()
+		{
+			if (this.DocumentStore != null)
+			{
+				return;
+			}
+
+			if (string.IsNullOrEmpty(this.ConnectionString))
+			{
+				throw new InvalidOperationException("Connection string is not specified.");
+			}
+
+			this.DocumentStore = new DocumentStore
+			{
+				Identifier = this.DatabaseName,
+				ConnectionStringName = this.ConnectionString
+			};
+
+			this.DocumentStore.Initialize();
 		}
 	}
 }
